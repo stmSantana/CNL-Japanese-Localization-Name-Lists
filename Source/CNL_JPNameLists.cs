ed0dc1ff6219eff26e1e1f4a416dfcbd4bb54f7a
stmSantana <stmtes5@gmail.com>
2018-02-15 12:00:43 +0900

    Add files via upload

---
+++ Source/CNL_JPNameLists.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Reflection;
+using UnityEngine;
+using ColossalFramework.UI;
+using ICities;
+
+namespace CNL_JPNameLists
+{
+public class CNL_JPNameLists: IUserMod
+{
+public string Name
+{
+get { return "CNL Japanese Localization Name Lists"; }
+}
+
+public string Description
+{
+get { return "This MOD doesn't work by itself. Please subscribe 'Custom Name Lists' Mod, Then, Enable both Mod. by stmSantana"; }
+}
+}
+} 
